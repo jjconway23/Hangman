77e0506ee3b52badc8ede744a54f56f19b4c18f9
Jacob <jacob.peat01@gmail.com>
2022-04-25 16:05:19 +0100

    Get words from random words api

---
+++ words.py
@@ -0,0 +1,10 @@
+import requests
+def getWords():
+    """
+    Gets words from api, returns list of words printed to the console
+    """
+    response = requests.get("https://random-word-api.herokuapp.com/word?number=10&length=7")
+    words = response.json()
+    print(words)
+
+getWords()
