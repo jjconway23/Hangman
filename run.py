aa616973d6a86744146dc9a8b45199da119b4dad
Jacob <jacob.peat01@gmail.com>
2022-04-25 17:40:48 +0100

    create method to display the the letter placeholders

--- run.py
+++ run.py
@@ -6,3 +6,6 @@ class Hangman:
         self.word = random.choice(getWords())
         self.display = ["_" for letter in self.word]
         self.guesses = 0
+    def show_word(self):
+        display = "".join(self.display)
+        print(f"Guess the word: {display}")
--- words.py
+++ words.py
@@ -3,7 +3,7 @@ def getWords():
     """
     Gets words from api, returns list of words printed to the console
     """
-    response = requests.get("https://random-word-api.herokuapp.com/word?number=10&length=7")
+    response = requests.get("https://random-word-api.herokuapp.com/word?number=5&length=7")
     words = response.json()
     return words
 
