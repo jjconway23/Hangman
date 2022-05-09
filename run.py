ac75e10d10bf6d6ec70c99a5c2e78ace53ac09ca
jjconway23 <jacob.peat01@gmail.com>
2022-05-09 08:11:53 +0000

    create function to update display of word after each guess

--- run.py
+++ run.py
@@ -13,18 +13,28 @@ class Hangman:
         self.word = random.choice(get_words())
         self.display = ["_" for letter in self.word]
         self.guesses = 0
+
     def show_word(self):
         """
         Displays the letter placeholder "_" in the terminal
         """
         display = "".join(self.display)
         print(f"Guess the word: {display}")
+
     def get_letter_index(self,guess):
         """
         checks if the guess entered is equal to a letter in the random word
         """
         letter_locations = []
-        for index,char in enumerate(list(self.word)):
+        for index ,char in enumerate(list(self.word)):
             if char == guess:
                 letter_locations.append(index)
         return letter_locations
+    
+    def update_view(self, index, letter):
+        """
+            update self.display at each index
+        """
+        for num in index:
+            self.display[num] = letter
+
